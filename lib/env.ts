bda1b722654e7e27558b69cb242079c69da86b34
dionisiusragil13 <dionisiusragil13@gmail.com>
2026-06-06 13:23:43 +0700

    fix

--- lib/env.ts
+++ lib/env.ts
@@ -4,5 +4,6 @@ dotenv.config();
 
 export const env = {
   GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  DATABASE_URL:process.env.DATABASE_URL
 };
